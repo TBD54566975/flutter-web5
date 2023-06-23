1bcefb7652d9475f695b2c751736c1a0882fd75f
Wes <wesbillman@users.noreply.github.com>
2023-06-23 09:06:12 -0700

    Add mocks of personas page

--- lib/src/routing/router.gr.dart
+++ lib/src/routing/router.gr.dart
@@ -8,47 +8,63 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i6;
-import 'package:wallet_exp/src/app_tabs.dart' as _i5;
-import 'package:wallet_exp/src/features/apps/apps_page.dart' as _i3;
+import 'package:auto_route/auto_route.dart' as _i8;
+import 'package:wallet_exp/src/app_tabs.dart' as _i6;
+import 'package:wallet_exp/src/features/apps/apps_page.dart' as _i4;
 import 'package:wallet_exp/src/features/contacts/contacts_page.dart' as _i1;
 import 'package:wallet_exp/src/features/credentials/credentials_page.dart'
-    as _i4;
-import 'package:wallet_exp/src/features/personas/personas_page.dart' as _i2;
+    as _i5;
+import 'package:wallet_exp/src/features/personas/create_persona_page.dart'
+    as _i3;
+import 'package:wallet_exp/src/features/personas/list_personas_page.dart'
+    as _i7;
+import 'package:wallet_exp/src/features/personas/view_persona_page.dart' as _i2;
 
-abstract class $AppRouter extends _i6.RootStackRouter {
+abstract class $AppRouter extends _i8.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i6.PageFactory> pagesMap = {
+  final Map<String, _i8.PageFactory> pagesMap = {
     ContactsRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i1.ContactsPage(),
       );
     },
-    PersonasRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+    ViewPersonaRoute.name: (routeData) {
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i2.PersonasPage(),
+        child: const _i2.ViewPersonaPage(),
+      );
+    },
+    CreatePersonaRoute.name: (routeData) {
+      return _i8.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i3.CreatePersonaPage(),
       );
     },
     AppsRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i3.AppsPage(),
+        child: const _i4.AppsPage(),
       );
     },
     CredentialsRoute.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i4.CredentialsPage(),
+        child: const _i5.CredentialsPage(),
       );
     },
     AppTabs.name: (routeData) {
-      return _i6.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i5.AppTabs(),
+        child: const _i6.AppTabs(),
+      );
+    },
+    ListPersonasRoute.name: (routeData) {
+      return _i8.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i7.ListPersonasPage(),
       );
     },
   };
@@ -56,8 +72,8 @@ abstract class $AppRouter extends _i6.RootStackRouter {
 
 /// generated route for
 /// [_i1.ContactsPage]
-class ContactsRoute extends _i6.PageRouteInfo<void> {
-  const ContactsRoute({List<_i6.PageRouteInfo>? children})
+class ContactsRoute extends _i8.PageRouteInfo<void> {
+  const ContactsRoute({List<_i8.PageRouteInfo>? children})
       : super(
           ContactsRoute.name,
           initialChildren: children,
@@ -65,27 +81,41 @@ class ContactsRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'ContactsRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i2.PersonasPage]
-class PersonasRoute extends _i6.PageRouteInfo<void> {
-  const PersonasRoute({List<_i6.PageRouteInfo>? children})
+/// [_i2.ViewPersonaPage]
+class ViewPersonaRoute extends _i8.PageRouteInfo<void> {
+  const ViewPersonaRoute({List<_i8.PageRouteInfo>? children})
       : super(
-          PersonasRoute.name,
+          ViewPersonaRoute.name,
           initialChildren: children,
         );
 
-  static const String name = 'PersonasRoute';
+  static const String name = 'ViewPersonaRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i3.AppsPage]
-class AppsRoute extends _i6.PageRouteInfo<void> {
-  const AppsRoute({List<_i6.PageRouteInfo>? children})
+/// [_i3.CreatePersonaPage]
+class CreatePersonaRoute extends _i8.PageRouteInfo<void> {
+  const CreatePersonaRoute({List<_i8.PageRouteInfo>? children})
+      : super(
+          CreatePersonaRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CreatePersonaRoute';
+
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i4.AppsPage]
+class AppsRoute extends _i8.PageRouteInfo<void> {
+  const AppsRoute({List<_i8.PageRouteInfo>? children})
       : super(
           AppsRoute.name,
           initialChildren: children,
@@ -93,13 +123,13 @@ class AppsRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'AppsRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i4.CredentialsPage]
-class CredentialsRoute extends _i6.PageRouteInfo<void> {
-  const CredentialsRoute({List<_i6.PageRouteInfo>? children})
+/// [_i5.CredentialsPage]
+class CredentialsRoute extends _i8.PageRouteInfo<void> {
+  const CredentialsRoute({List<_i8.PageRouteInfo>? children})
       : super(
           CredentialsRoute.name,
           initialChildren: children,
@@ -107,13 +137,13 @@ class CredentialsRoute extends _i6.PageRouteInfo<void> {
 
   static const String name = 'CredentialsRoute';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i5.AppTabs]
-class AppTabs extends _i6.PageRouteInfo<void> {
-  const AppTabs({List<_i6.PageRouteInfo>? children})
+/// [_i6.AppTabs]
+class AppTabs extends _i8.PageRouteInfo<void> {
+  const AppTabs({List<_i8.PageRouteInfo>? children})
       : super(
           AppTabs.name,
           initialChildren: children,
@@ -121,5 +151,19 @@ class AppTabs extends _i6.PageRouteInfo<void> {
 
   static const String name = 'AppTabs';
 
-  static const _i6.PageInfo<void> page = _i6.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i7.ListPersonasPage]
+class ListPersonasRoute extends _i8.PageRouteInfo<void> {
+  const ListPersonasRoute({List<_i8.PageRouteInfo>? children})
+      : super(
+          ListPersonasRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ListPersonasRoute';
+
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
