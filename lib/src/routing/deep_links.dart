92dccf5bf6bfa18c0ae368ec4e939f33d44388af
Wes <wesbillman@users.noreply.github.com>
2023-06-27 14:46:59 -0700

    Minor cleanup and tests change

--- lib/src/routing/deep_links.dart
+++ lib/src/routing/deep_links.dart
@@ -52,25 +52,3 @@ class DeepLinks extends HookConsumerWidget {
     return widget;
   }
 }
-
-// class DeepLinks {
-//   final logger = Logger('DeepLinks');
-
-//   Future<void> initDeepLinks() async {
-//     final initialLink = await getInitialLink();
-//     if (initialLink != null) {
-//       logger.info('Got a deep link: $initialLink');
-//     }
-//     uriLinkStream.listen((Uri? uri) async {
-//       logger.info('Got a deep link: ${uri?.scheme}');
-//       logger.info('Got a deep link: $uri');
-//       final Uri url = Uri.parse('https://jttz2d.csb.app#mykey=123');
-//       logger.info(url);
-//       if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
-//         throw Exception('Could not launch $url');
-//       }
-//     }, onError: (err) {
-//       logger.severe('Got an error! $err');
-//     });
-//   }
-// }
--- test/app_test.dart
+++ test/app_test.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:wallet_exp/src/app.dart';
+import 'package:wallet_exp/src/features/personas/create_persona_page.dart';
 import 'package:wallet_exp/src/features/personas/list_personas_page.dart';
 import 'package:wallet_exp/src/features/personas/personas_providers.dart';
 import 'package:wallet_exp/src/features/web5/web5_service.dart';
@@ -53,6 +55,29 @@ void main() {
 
     await tester.pumpAndSettle();
     expect(find.byType(ListPersonasPage), findsOneWidget);
+    verify(() => didBox.isEmpty).called(1);
+    verify(() => didBox.values).called(1);
+  });
+
+  testWidgets('Tapping create button opens create person page',
+      (WidgetTester tester) async {
+    when(() => didBox.isEmpty).thenReturn(false);
+    when(() => didBox.values).thenReturn([]);
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          web5Provider.overrideWithValue(web5Service),
+          didBoxProvider.overrideWithValue(didBox),
+        ],
+        child: App(),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+    await tester.tap(find.byType(FilledButton));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(CreatePersonaPage), findsOneWidget);
     verify(() => didBox.isEmpty).called(1);
     verify(() => didBox.values).called(1);
   });
